9b82b2d6f11d8c533c7c5cd5f6f982f45f6f20bf
Frank Hermes <frankhermes@cox.net>
2019-07-09 18:41:18 -0700

    Add venv

--- action-app_voicekit.py
+++ action-app_voicekit.py
@@ -11,8 +11,8 @@ import grove.grove_relay
 import grove.grove_temperature_humidity_sensor_sht3x
 import serial
 
-'''
-# import board
+
+import board
 
 import adafruit_dotstar as dotstar
 
@@ -47,7 +47,7 @@ dots[17] = (255, 255, 255)
 dots[24] = (255, 255, 255)
 dots[22] = (255, 255, 255)
 dots[31] = (255, 255, 255)
-'''
+
 CONFIG_INI = "config.ini"
 
 MQTT_IP_ADDR = "localhost"
